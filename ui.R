d892a62f9c9f994a9312484cbb13ee17fe97cb50
lboro-rdm <rdm@lboro.ac.uk>
2025-04-04 14:49:48 +0100

    updated banner

--- ui.R
+++ ui.R
@@ -9,35 +9,55 @@ library(dplyr)
 ui <- tags$html(
   lang = "en",
   fluidPage(
+    style = "padding: 0px; margin: 0px;",
     tags$head(
-      tags$title("WEDC, Loughborough University: Books and Manuals"),
+      tags$title("WEDC: Books and Manuals"),
       tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
     ),
     
+    # Black banner
     tags$div(
-      class = "wedc-title-container",
-      tags$span(class = "wedc-title", "WEDC, Loughborough University: Books and Manuals"),
-      tags$a(
-        href = "https://lboro.ac.uk",
-        target = "_blank",  # Opens in a new tab
-        tags$img(src = "logo.png", class = "wedc-logo", alt = "WEDC Logo")
+      class = "black-banner",
+      tags$div(
+        class = "banner-content",
+        tags$a(
+          href = "https://www.lboro.ac.uk",
+          target = "_blank",
+          tags$img(src = "logo.png", class = "uni-logo", alt = "University Logo")
+        ),
+        tags$span("School of Architecture, Building and Civil Engineering")
       )
+    ),
+    
+    # Blue banner
+    tags$div(
+      class = "blue-banner",
+      tags$div(
+        class = "banner-content",
+        tags$span("Water Engineering and Development Centre"),
+        tags$a(
+          href = "https://www.lboro.ac.uk/research/wedc/publications-and-resources/",
+          class = "return-link",
+          "< Return to Publications and resources"
+        )
+      )
+    ),
+    
+    # Title section
+    tags$div(
+      class = "white-banner",
+      tags$h1("Books and Manuals")
     ),
     
     sidebarLayout(
       sidebarPanel(
+        style = "margin-left: 20px; padding-right: 20px;",
         selectInput("collectionSelect", "Select by Collection:", choices = NULL),
         textInput("authorSearch", "Search by Author:", placeholder = "Enter author\'s name"),
         textInput("titleSearch", "Search by Title:", placeholder = "Enter book or manual title"),
         p(),
         p("Water Engineering and Development Centre (WEDC) produces and disseminates quality, relevant and accessible knowledge products to meet the needs of academics, policymakers and practitioners working in various aspects of water engineering and development."),
-        p("Books and manuals represent our substantial body of knowledge in water management, engineering and other international development-related subjects developed over 50 years."),
-        p(),
-        p(tags$a(
-          href = "https://www.lboro.ac.uk/research/wedc/publications-and-resources/",
-          "View our other publications.",
-          target = "_blank"
-        ))
+        p("Books and manuals represent our substantial body of knowledge in water management, engineering and other international development-related subjects developed over 50 years.")
       ),
       mainPanel(
         withSpinner(uiOutput("bookDetails"), type = 3, color = "#009BC9", color.background = "#FFFFFF")
